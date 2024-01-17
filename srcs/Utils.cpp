21d5bc9fcb68809dc602e389cd5fca20374837ad
mbrettsc <burakmartin007@outlook.com>
2024-01-17 16:42:18 +0300

    PRIVMSG.cpp and write event added

--- srcs/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "../includes/Utils.hpp"
-#include <unistd.h>
-
-void Utils::writeMessage(int socket, std::string const& message)
-{
-    if ((write(socket, message.c_str(), message.length())) < 0)
-        std::cout << "Message cannot send!" << std::endl;
-}
--- srcs/commands/CAP.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../includes/Server.hpp"
-
-void Server::Cap(std::vector<std::string>& param, Client& cli)
-{
-    if (param[0] == "LS" && param.size() == 1)
-        cli.isCap = 1;
-}
